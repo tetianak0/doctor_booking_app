4fac2f69dc9e96f3ed14c25d0232d3f42fea8b59
JuliaPashkova <jjstudio1cn@gmail.com>
2024-08-08 08:50:50 +0200

    pom add

--- src/main/java/doctorBookingApp/controller/HalloController.java
+++ src/main/java/doctorBookingApp/controller/HalloController.java
@@ -1,8 +1,6 @@
 package doctorBookingApp.controller;
 
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 
-@Api(value = "TestController", description = "REST API для приветствия")
+//@Api(value = "TestController", description = "REST API для приветствия")
 public class HalloController {
 
     @GetMapping("/hallo")
-    @ApiOperation(value = "Скажи привет", notes = "Возвращает строку 'hallo'")
+//    @ApiOperation(value = "Скажи привет", notes = "Возвращает строку 'hallo'")
     public String sayHallo() {
         return "hallo";
     }
