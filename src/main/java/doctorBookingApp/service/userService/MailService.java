028fb15df5e4bf469557b5c6e313adb13ea1c27f
NataliBurlakova <156796031+NataliBurlakova@users.noreply.github.com>
2024-08-08 04:25:53 +0300

    Update MailService.java

--- src/main/java/doctorBookingApp/service/userService/MailService.java
+++ src/main/java/doctorBookingApp/service/userService/MailService.java
@@ -44,12 +44,6 @@ public class MailService {
 
 
 
-
-
-
-// КОММЕНТАРИИ К КОДУ
-
-
 //ОТПРАВКА НА ПОЧТУ ПРОСТО КОДА. Как вариант: пользователь может ввести его в регистрационную форму и тем самым подтвердить свою личность
 //    public void sendConfirmationEmail(User user, String confirmationCode) {
 //        SimpleMailMessage mailMessage = new SimpleMailMessage();
@@ -61,57 +55,6 @@ public class MailService {
 //    }
 
 
-
-//    public void sendConfirmationEmailWithHTML(User user) throws MessagingException {
-//
-//        String confirmationUrl = "http://localhost:8080/api/users/confirm?confirmationCode=" + user.getConfirmationCode();
-//
-//        /*        localhost:8080 - куда отправляем
-//        api/users/confirm?confirmationCode= - путь        */
-//
-//        String subject = "Registration Confirmation Code";
-//        String textBody = "<p> Click the link below to confirm email:</p>" +
-//                "<a href=\"" + confirmationUrl + "\">Confirm Email </a>"; //размещаем ссылку
-//
-//        // обратный слэш (\) ставится для того, чтобы кавычки воспринимались как часть текста. А он воспринмается как спецкод
-//
-//        /*
-//         для того чтобы в текст включить что-то отличное от обычного текста, в библиотеке mail существует два класса:
-//         MimeMessage  и MimeMessageHelper
-//         */
-//
-//        MimeMessage message = mailSender.createMimeMessage();
-//
-//
-//        /*MimeMessage — это класс, который из Java Mail, и он используется, если нам нужно создать и отправить сообщение, которое будет содерживать различные форматы
-//        контента (HTML, какие-то вложения, картинки и так далее).
-//        Особенности:
-//                     - многоформатность
-//                     - может быть множество получателей (поддержка нескольких получателей с помощью там полей там CC, BCC и т.д.; копии, скрытые копии и так далее)
-//                     - позволяет вручную настраивать, задавать и изменить заголовки сообщения, если она нужна.
-//
-//         */
-//
-//        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-//
-//        /* Helper это по сути помогатель. Для упрощения работы. То есть он позволяет легко устанавливать HTML-сообщения, добавлять вложения, изображения, так далее.
-//         Поддерживает многобайтовые символы, то есть поддержку как бы в самом коде
-//
-//          */
-//
-//        helper.setTo(user.getEmail());
-//        helper.setSubject(subject);
-//        helper.setText(textBody, true);
-//
-//        mailSender.send(message);
-//
-//        SimpleMailMessage mailMessage = new SimpleMailMessage();
-//        mailMessage.setFrom("doctorbookingBoss@gmail.com");
-//        mailMessage.setTo(user.getEmail());
-//        mailMessage.setSubject("Registration Confirmation Code");
-//        mailMessage.setText("Please confirm your registration with code: " + user.getConfirmationCode());
-//        mailSender.send(mailMessage);
-//    }
 
 }
 
