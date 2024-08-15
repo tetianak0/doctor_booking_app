52263f6c0998560c3be95e28006be87615f5383e
NataliBurlakova <natali.burlakova.2022@gmail.com>
2024-08-15 15:39:25 +0300

    update

---
+++ src/main/java/doctorBookingApp/exeption/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package doctorBookingApp.exeption;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RestException.class)
+    public ResponseEntity<Object> handleRestException(RestException ex) {
+        // Возвращаем сообщение и статус из RestException
+        return new ResponseEntity<>(ex.getMessage(), ex.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralException(Exception ex) {
+        // Обработка общих исключений, возвращаем статус 500
+        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
--- src/main/java/doctorBookingApp/exeption/RestException.java
+++ src/main/java/doctorBookingApp/exeption/RestException.java
@@ -1,6 +1,24 @@
 package doctorBookingApp.exeption;
 
-public class RestException extends Throwable {
-    public RestException(Object p0, Object p1) {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+public class RestException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public RestException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public RestException(Object p0, Object p1, HttpStatus status) {
+        this.status = status;
+    }
+
+
+
+    public HttpStatusCode getStatus() {
+        return status;
     }
 }
