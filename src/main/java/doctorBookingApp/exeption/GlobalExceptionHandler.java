77ffc36424aec6398976308304bf2f54f96b17d0
NataliBurlakova <natali.burlakova.2022@gmail.com>
2024-08-15 15:41:10 +0300

    update

--- src/main/java/doctorBookingApp/exeption/GlobalExceptionHandler.java
+++ src/main/java/doctorBookingApp/exeption/GlobalExceptionHandler.java
@@ -11,12 +11,14 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RestException.class)
     public ResponseEntity<Object> handleRestException(RestException ex) {
+
         // Возвращаем сообщение и статус из RestException
         return new ResponseEntity<>(ex.getMessage(), ex.getStatus());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGeneralException(Exception ex) {
+
         // Обработка общих исключений, возвращаем статус 500
         return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
