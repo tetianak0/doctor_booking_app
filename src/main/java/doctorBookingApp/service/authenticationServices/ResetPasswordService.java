65a87ec8d53a524fefe41cce35927b1a9ce98ffe
JuliaPashkova <jjstudio1cn@gmail.com>
2024-08-20 19:09:50 +0200

    Update ResetPasswordService.java

--- src/main/java/doctorBookingApp/service/authenticationServices/ResetPasswordService.java
+++ src/main/java/doctorBookingApp/service/authenticationServices/ResetPasswordService.java
@@ -32,7 +32,7 @@ public class ResetPasswordService {
         user.setTokenForResetPassword(tokenForPassword);
         userRepository.save(user);
 
-        String resetURL = "http://localhost:8080/api/auth-for-reset/reset-password?token=" + tokenForPassword;
+        String resetURL = "http://localhost:5173/reset-password?token=" + tokenForPassword;
 
         String subjectOfLetter = "Password reset";
         String textBody = "<p> Click the link below to reset password:</p>" +
