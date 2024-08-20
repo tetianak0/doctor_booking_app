90a1978617d43daa0019e29b3fca54005367827f
JuliaPashkova <jjstudio1cn@gmail.com>
2024-08-20 22:52:10 +0200

    Update DoctorProfileRepository.java

--- src/main/java/doctorBookingApp/repository/DoctorProfileRepository.java
+++ src/main/java/doctorBookingApp/repository/DoctorProfileRepository.java
@@ -2,12 +2,15 @@ package doctorBookingApp.repository;
 
 
 import doctorBookingApp.entity.DoctorProfile;
+import doctorBookingApp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DoctorProfileRepository extends JpaRepository<DoctorProfile, Long> {
     List<DoctorProfile> findByDepartmentId(Long departmentId);
+    Optional<DoctorProfile> getDoctorProfileById(Long id);
 }
